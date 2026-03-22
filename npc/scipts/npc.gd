838e2c8c07e7158397cca5af59f1ab42543ab813
Jack Beeken <beekenj@gmail.com>
2026-03-22 11:57:42 -0600

    introduced npcs

---
+++ npc/scipts/npc.gd
@@ -0,0 +1,60 @@
+@tool
+@icon("res://npc/icons/npc.svg")
+extends CharacterBody2D
+class_name NPC
+
+signal do_behavior_enabled
+
+var state : String = "idle"
+var direction : Vector2 = Vector2.DOWN
+var direction_name : String = "down"
+var do_behavior : bool = true
+
+@export var npc_resource : NPCResource : set = _set_npc_resource
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var animation: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	setup_npc()
+	if Engine.is_editor_hint():
+		return
+	do_behavior_enabled.emit()
+
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+
+
+func update_animation() -> void:
+	animation.play(state + "_" + direction_name)
+
+
+func update_direction(target_position : Vector2) -> void:
+	direction = global_position.direction_to(target_position)
+	update_direction_name()
+	if direction_name == "side" and direction.x < 0:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+	
+
+func update_direction_name() -> void:
+	var threshold : float = 0.45
+	if direction.y < -threshold:
+		direction_name= "up"
+	elif direction.y > threshold:
+		direction_name = "down"
+	elif direction.x > threshold or direction.x < -threshold:
+		direction_name = "side"
+
+
+func setup_npc() -> void:
+	if npc_resource and sprite:
+		sprite.texture = npc_resource.sprite
+
+
+func _set_npc_resource(_npc : NPCResource) -> void:
+	npc_resource = _npc
+	setup_npc()
