cef5c23700aa3ee2f7ae7f5db73fde7dbb67dcfa
Jack Beeken <beekenj@gmail.com>
2026-07-20 15:49:26 -0600

    boss battle: finished

--- Levels/Dungeon01/dark_wizard/scripts/dark_wizard_boss.gd
+++ Levels/Dungeon01/dark_wizard/scripts/dark_wizard_boss.gd
@@ -2,14 +2,19 @@ extends Node2D
 class_name DarkWizardBoss
 
 const ENERGY_EXPLOSION_SCENE : PackedScene = preload("res://Levels/Dungeon01/dark_wizard/energy_explosion.tscn")
+const ENERGY_BALL_SCENE : PackedScene = preload("res://Levels/Dungeon01/dark_wizard/energy_orb.tscn")
 
 @export var max_hp : int = 10
 var hp : int = 10
 
 var audio_hurt : AudioStream = preload("res://Levels/Dungeon01/dark_wizard/audio/boss_hurt.wav")
+var audio_shoot : AudioStream = preload("res://Levels/Dungeon01/dark_wizard/audio/boss_fireball.wav")
 
 var current_position : int = 0
 var positions : Array[Vector2]
+var beam_attacks : Array[BeamAttack]
+
+var damnage_count : int = 0
 
 @onready var animation_player: AnimationPlayer = $BossNode/AnimationPlayer
 @onready var animation_player_damaged: AnimationPlayer = $BossNode/AnimationPlayer_Damaged
@@ -26,18 +31,27 @@ var positions : Array[Vector2]
 @onready var hand_02_up: Sprite2D = $BossNode/CloakSprite/Hand02_UP
 @onready var hand_01_side: Sprite2D = $BossNode/CloakSprite/Hand01_SIDE
 @onready var hand_02_side: Sprite2D = $BossNode/CloakSprite/Hand02_SIDE
+@onready var door_block: TileMapLayer = $'../DoorBlock'
+
 
 
 func _ready() -> void:
-    
+    persistent_data_handler.get_value()
+    if persistent_data_handler.value == true:
+        door_block.enabled = false
+        queue_free()
+        return
+
     hp = max_hp
 
     hit_box.damaged.connect(damage_taken)
 
     for c in $PositionTargets.get_children():
         positions.append(c.global_position)
-    print(positions)
     $PositionTargets.visible = false
+
+    for b in $BeamAttacks.get_children():
+        beam_attacks.append(b)
 
     teleport(0)
 
@@ -56,8 +70,10 @@ func _process(_delta: float) -> void:
 func teleport(_location : int) -> void:
     animation_player.play("disappear")
     enable_hit_boxes(false)
+    damnage_count = 0
 
-    # shoot fireball
+    if hp < max_hp:
+        shoot_orb()
     await get_tree().create_timer(1).timeout
 
     boss_node.global_position = positions[_location]
@@ -71,8 +87,19 @@ func teleport(_location : int) -> void:
 func idle() -> void:
     enable_hit_boxes()
     
-    animation_player.play("idle")
-    await animation_player.animation_finished
+    if randf() <= float(hp) / float(max_hp):
+        animation_player.play("idle")
+        await animation_player.animation_finished
+        if hp < 1:
+            return
+
+    if damnage_count < 1:
+        energy_beam_attack()
+        animation_player.play("cast_spell")
+        await animation_player.animation_finished
+    
+    if hp < 1:
+        return
 
     var _t : int = current_position
     while _t == current_position:
@@ -106,12 +133,47 @@ func update_animations() -> void:
             boss_node.scale = Vector2(-1, 1)
 
 
+func energy_beam_attack() -> void:
+    var _b : Array[int]
+
+    match current_position:
+        0, 2:
+            if current_position == 0:
+                _b.append(0)
+                _b.append(randi_range(1, 2))
+            else:
+                _b.append(2)
+                _b.append(randi_range(0, 1))
+            if hp < 5:
+                _b.append(randi_range(3, 5))
+        1, 3:
+            if current_position == 1:
+                _b.append(5)
+                _b.append(randi_range(3, 4))
+            else:
+                _b.append(3)
+                _b.append(randi_range(4, 5))
+            if hp < 5:
+                _b.append(randi_range(0, 2))
+
+    for b in _b:
+        beam_attacks[b].attack()
+
+
+func shoot_orb() -> void:
+    var eb : Node2D = ENERGY_BALL_SCENE.instantiate()
+
+    eb.global_position = boss_node.global_position + Vector2(0, -34)
+    get_parent().add_child.call_deferred(eb)
+    play_audio(audio_shoot)
+
 
 func damage_taken(_hurt_box : HurtBox) -> void:
     if animation_player_damaged.current_animation == "damaged" or _hurt_box.damage == 0:
         return
     play_audio(audio_hurt)    
     hp = clampi(hp - _hurt_box.damage, 0, max_hp)
+    damnage_count += 1
     # Update Boss Health Bar
     animation_player_damaged.play("damaged")
     animation_player_damaged.seek(0)
@@ -131,7 +193,7 @@ func defeat() -> void:
     enable_hit_boxes(false)
     persistent_data_handler.set_value()
     await animation_player.animation_finished
-    # reopen the room
+    door_block.enabled = false
 
 
 func enable_hit_boxes(_v : bool = true) -> void:
