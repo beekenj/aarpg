878c4f34948c4c44a2010444dde64b88f5bfc843
Jack Beeken <beekenj@gmail.com>
2026-09-24 16:34:24 -0600

    leveling bug fix

--- 00_Globals/global_player_manager.gd
+++ 00_Globals/global_player_manager.gd
@@ -12,6 +12,7 @@ var interact_handled : bool = true
 var player : Player
 var player_spawned : bool = false
 var level_requirements : Array[int] = [0, 50, 100, 200, 400, 800, 1500, 3000, 6000, 12000, 25000]
+# var level_requirements : Array[int] = [0, 5, 10, 20, 40]
 
 
 func _ready() -> void:
@@ -34,11 +35,18 @@ func set_health(hp : int, max_hp : int) -> void:
 func reward_xp(_xp : int) -> void:
 	player.xp += _xp
 	# check for level advancement
+	check_for_level_advance()
+
+
+func check_for_level_advance() -> void:
+	if player.level >= level_requirements.size():
+		return
 	if player.xp >= level_requirements[player.level]:
 		player.level += 1
 		player.attack += 1
 		player.defense += 1
 		player_leveled_up.emit()
+		check_for_level_advance()
 
 
 func set_player_position(_new_pos : Vector2) -> void:
